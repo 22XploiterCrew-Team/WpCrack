30b5452734da455fc023165861cfdcd4a18aca28
RandsX <68506532+RandsX@users.noreply.github.com>
2020-11-13 14:59:52 +0700

    update wp-crack.py

--- wp-crack.py
+++ wp-crack.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Author : RandsX
+Team   : 22XploiterCrew
+"""
+
 _E='[{}Trying{}] {}::{}'
 _D='(http|https)://[\\w-]+(.[\\w-]+)+\\S*'
 _C=False
